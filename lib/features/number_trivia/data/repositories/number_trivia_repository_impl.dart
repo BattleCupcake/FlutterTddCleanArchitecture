39587f3b7b519b14c7e70715556a7d5a0a7b9263
Max <maxim.ananin@bravedevelopers.com>
2020-05-31 20:20:35 +0400

    NetworkInfo Implementation

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_tdd_clean_architecture/core/error/exceptions.dart';
+import 'package:flutter_tdd_clean_architecture/core/network/network_info.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_tdd_clean_architecture/core/error/failures.dart';
-import 'package:flutter_tdd_clean_architecture/core/platform/network_info.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
